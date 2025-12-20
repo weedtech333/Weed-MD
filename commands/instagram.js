83811415a3063d898f8cbb166455f794eb7dd9d5
weedtech333 <weedtech333@gmail.com>
2025-12-20 17:52:19 -0500

    Update instagram.js

--- commands/instagram.js
+++ commands/instagram.js
@@ -113,12 +113,12 @@ async function instagramCommand(sock, chatId, message) {
                     await sock.sendMessage(chatId, {
                         video: { url: mediaUrl },
                         mimetype: "video/mp4",
-                        caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝙎𝙄𝙇𝘼 𝙈𝘿 -𝗕𝗢𝗧"
+                        caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝗪𝗲𝗲𝗱-𝗠𝗗 𝗕𝗢𝗧"
                     }, { quoted: message });
                 } else {
                     await sock.sendMessage(chatId, {
                         image: { url: mediaUrl },
-                        caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝙎𝙄𝙇𝘼 𝙈𝘿"
+                        caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝗪𝗲𝗲𝗱 𝗠𝗗"
                     }, { quoted: message });
                 }
                 
