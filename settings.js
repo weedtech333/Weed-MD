9082525d83816051043b80622dad434ad13ef6d4
weedtech333 <weedtech333@gmail.com>
2025-12-20 16:37:40 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ const settings = {
   description: "This is a bot for managing group commands and automating tasks.",
   
   // Update Settings
-  updateZipUrl: "https://github.com/Sila-Md/SILA-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/weedtech333/Weed-MD/archive/refs/heads/main.zip",
   
   // Channel & Group Settings
   newsletterJid: "120363402325089913@newsletter",
