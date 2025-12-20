9fc256ef4f64695e90c423d2049399a154a68316
weedtech333 <weedtech333@gmail.com>
2025-12-20 18:55:56 -0500

    Update stickercrop.js

--- commands/stickercrop.js
+++ commands/stickercrop.js
@@ -37,7 +37,7 @@ async function stickercropCommand(sock, chatId, message) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363422610520277@newsletter',
-                    newsletterName: 'SILA MD',
+                    newsletterName: 'WEED MD',
                     serverMessageId: -1
                 }
             }
@@ -59,7 +59,7 @@ async function stickercropCommand(sock, chatId, message) {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363422610520277@newsletter',
-                        newsletterName: 'SILA MD',
+                        newsletterName: 'WEED MD',
                         serverMessageId: -1
                     }
                 }
@@ -187,7 +187,7 @@ async function stickercropCommand(sock, chatId, message) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363422610520277@newsletter',
-                    newsletterName: 'SILA MD',
+                    newsletterName: 'WEED MD',
                     serverMessageId: -1
                 }
             }
