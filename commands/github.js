be3ddadff836143ff107b378882f5d91786f7479
weedtech333 <weedtech333@gmail.com>
2025-12-25 19:17:49 -0500

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -13,20 +13,20 @@ async function githubCommand(sock, chatId, message) {
       }
     });
 
-    const res = await fetch('https://api.github.com/repos/weed-Md/WEED-MD');
+    const res = await fetch('https://github.com/weedtech333/Weed-MD');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
-    let txt = `*╭━━━〔 🎀 𝙶𝙸𝚃𝙷𝚄𝙱 𝙸𝙽𝙵𝙾 🎀 〕━━━┈⊷*\n`;
-    txt += `*┃🎀│ 𝚁𝙴𝙿𝙾 :❯ ${json.name}*\n`;
-    txt += `*┃🎀│ 𝚆𝙰𝚃𝙲𝙷𝙴𝚁𝚂 :❯ ${json.watchers_count}*\n`;
-    txt += `*┃🎀│ 𝚂𝙸𝚉𝙴 :❯ ${(json.size / 1024).toFixed(2)} MB*\n`;
-    txt += `*┃🎀│ 𝚄𝙿𝙳𝙰𝚃𝙴𝙳 :❯ ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}*\n`;
-    txt += `*┃🎀│ 𝙵𝙾𝚁𝙺𝚂 :❯ ${json.forks_count}*\n`;
-    txt += `*┃🎀│ 𝚂𝚃𝙰𝚁𝚂 :❯ ${json.stargazers_count}*\n`;
-    txt += `*┃🎀│ 𝚄𝚁𝙻 :❯ ${json.html_url}*\n`;
+    let txt = `*╭━━━〔 ⚙️ 𝙶𝙸𝚃𝙷𝚄𝙱 𝙸𝙽𝙵𝙾 ⚙️ 〕━━━┈⊷*\n`;
+    txt += `*┃🏷│ 𝚁𝙴𝙿𝙾 :❯ ${json.name}*\n`;
+    txt += `*┃🏷│ 𝚆𝙰𝚃𝙲𝙷𝙴𝚁𝚂 :❯ ${json.watchers_count}*\n`;
+    txt += `*┃🏷│ 𝚂𝙸𝚉𝙴 :❯ ${(json.size / 1024).toFixed(2)} MB*\n`;
+    txt += `*┃🏷│ 𝚄𝙿𝙳𝙰𝚃𝙴𝙳 :❯ ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}*\n`;
+    txt += `*┃🏷│ 𝙵𝙾𝚁𝙺𝚂 :❯ ${json.forks_count}*\n`;
+    txt += `*┃🏷│ 𝚂𝚃𝙰𝚁𝚂 :❯ ${json.stargazers_count}*\n`;
+    txt += `*┃🏷│ 𝚄𝚁𝙻 :❯ ${json.html_url}*\n`;
     txt += `*╰━━━━━━━━━━━━━━━┈⊷*\n\n`;
-    txt += `*🦄 𝙶𝙸𝚃𝙷𝚄𝙱 𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝚈 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽 🎀*`;
+    txt += `*🦄 𝙶𝙸𝚃𝙷𝚄𝙱 𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝚈 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽 🤩*`;
 
     // Use the local asset image
     const imgPath = path.join(__dirname, '../assets/bot_image.jpg');
@@ -40,9 +40,9 @@ async function githubCommand(sock, chatId, message) {
   } catch (error) {
     console.error('Error in github command:', error);
     await sock.sendMessage(chatId, { 
-      text: '*╭━━━〔 🎀 𝙶𝙸𝚃𝙷𝚄𝙱 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 🎀 〕━━━┈⊷*\n' +
-            '*┃🎀│ 𝚂𝚃𝙰𝚃𝚄𝚂 :❯ 𝙴𝚁𝚁𝙾𝚁*\n' +
-            '*┃🎀│ 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 :❯ 𝙵𝙰𝙸𝙻𝙴𝙳 𝚃𝙾 𝙵𝙴𝚃𝙲𝙷 𝙳𝙰𝚃𝙰*\n' +
+      text: '*╭━━━〔 ⚙️ 𝙶𝙸𝚃𝙷𝚄𝙱 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 ⚙️ 〕━━━┈⊷*\n' +
+            '*┃🏷│ 𝚂𝚃𝙰𝚃𝚄𝚂 :❯ 𝙴𝚁𝚁𝙾𝚁*\n' +
+            '*┃🏷│ 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 :❯ 𝙵𝙰𝙸𝙻𝙴𝙳 𝚃𝙾 𝙵𝙴𝚃𝙲𝙷 𝙳𝙰𝚃𝙰*\n' +
             '*╰━━━━━━━━━━━━━━━┈⊷*'
     }, { quoted: message });
   }
