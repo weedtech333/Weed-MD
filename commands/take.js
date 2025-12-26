533c4ff9f7bc598b37e3b0b49f54cd713b842b2f
weedtech333 <weedtech333@gmail.com>
2025-12-25 20:12:19 -0500

    Update take.js

--- commands/take.js
+++ commands/take.js
@@ -14,7 +14,7 @@ async function takeCommand(sock, chatId, message, args) {
         }
 
         // Get the packname from args or use default
-        const packname = args.join(' ') || 'Knight Bot';
+        const packname = args.join(' ') || 'Weed MD';
 
         try {
             // Download the sticker
