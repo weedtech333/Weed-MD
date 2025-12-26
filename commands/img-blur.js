773f4573c6f389c58162912bbe322e8acd8aca6c
weedtech333 <weedtech333@gmail.com>
2025-12-25 19:21:50 -0500

    Update img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,7 +65,7 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363422610520277@newsletter',
+                    newsletterJid: '120363407561123100@newsletter',
                     newsletterName: 'WEED MD',
                     serverMessageId: -1
                 }
