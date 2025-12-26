58b5692403a7bf057243daa3356c241f04a3653a
weedtech333 <weedtech333@gmail.com>
2025-12-25 20:02:51 -0500

    Update simp.js

--- commands/simp.js
+++ commands/simp.js
@@ -37,7 +37,7 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363422610520277@newsletter',
+                    newsletterJid: '120363407561123100@newsletter',
                     newsletterName: 'WEED MD',
                     serverMessageId: -1
                 }
@@ -52,7 +52,7 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363422610520277@newsletter',
+                    newsletterJid: '120363407561123100@newsletter',
                     newsletterName: 'WEED MD',
                     serverMessageId: -1
                 }
