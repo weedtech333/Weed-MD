c03ffef5f3102f3b9991a95405daa85daec9a34d
weedtech333 <weedtech333@gmail.com>
2026-01-15 09:52:53 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 const settings = {
-  'packname': "𝙕𝙖𝙣𝙞𝙩𝙨𝙪 𝙗𝙤𝙩",
-  'author': 'CHRIS GAAJU',
-  'botName': "𝙕𝙖𝙣𝙞𝙩𝙨𝙪 𝙗𝙤𝙩",
-  'botOwner': "CHRIS GAAJU",
-  'ownerNumber': "2348069675806",
+  'packname': "𝗪𝗲𝗲𝗱-𝗠𝗗",
+  'author': 'W𝚎𝚎𝚍 Ｄ𝚎𝚟',
+  'botName': "𝗪𝗲𝗲𝗱-𝗠𝗗",
+  'botOwner': "Ｄ𝚎𝚟 𝚆𝚎𝚎𝚍",
+  'ownerNumber': "50939032060",
   'giphyApiKey': "qnl7ssQChTdPjsKta2LMaGXz303tq",
   'commandMode': 'public',
   'maxStoreMessages': 0x14,
   'storeWriteInterval': 0x2710,
   'description': "This is a bot for managing group commands and automating tasks.",
   'version': "1.0.0",
-  'updateZipUrl': 'https://github.com/qwerghjkkl/Zenitsu-BOT/archive/refs/heads/main.zip'
+  'updateZipUrl': 'https://github.com/weedtech333/Weed-MD/archive/refs/heads/main.zip'
 };
 module.exports = settings;
