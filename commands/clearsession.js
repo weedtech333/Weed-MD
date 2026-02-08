4fdd1c46e0e0d265c76cbbfd10a5348fa0fecd1a
weedtech333 <weedtech333@gmail.com>
2026-02-08 17:41:36 -0500

    Update clearsession.js

--- commands/clearsession.js
+++ commands/clearsession.js
@@ -7,8 +7,8 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363422610520277@newsletter',
-            newsletterName: 'SILA MD',
+            newsletterJid: '120363407561123100@newsletter',
+            newsletterName: 'WEED MD',
             serverMessageId: -1
         }
     }
